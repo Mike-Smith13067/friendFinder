1f050fdb04db564dd88850f65da33b0252e5ea64
Mike Smith <hockeydad66@live.com>
2020-01-06 10:17:34 -0700

    added js files to app folder

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('data'));
+app.use(express.static('public'));
+app.use(express.static('routing'));
+app.use('/jsfiles', express.static(path.join(__dirname, '/app/assets/javascript')));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, () =>{
+    console.log('App listening on port: ' + PORT);
+});
