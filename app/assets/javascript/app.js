feba240809104d56225a02f348769a4cf2d0baac
Mike Smith <hockeydad66@live.com>
2020-01-11 09:50:17 -0700

    more work

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('#submit').on('click', function(event){
         event.preventDefault();
-        console.log("click");
+        console.log("click read");
         var name = $('#name').val().trim();
-        var photo = $("photo").val().trim()
+        var photo = $("#photo").val().trim()
         var question1 = $('#q1').val();
         var question2 = $('#q2').val();
         var question3 = $('#q3').val();
@@ -19,15 +19,16 @@ $(document).ready(function(){
         $.post("/api/friends",
         {
             name: name,
+            photo: photo,
             scores: [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
         },
+        
         function(data, status){
             console.log("Data: " + data + "\nStatus: " + status);
-
             $("myModal").modal();
             $("#match").append("<p>" + data.name + "</p>");
             $("#match").append("<img class = 'matchimage' src= '" + data.photo + "' alt = 'photo'>");
-            $(".custo-select").val("");
+            $(".custom-select").val("");
         
         });
     }else {
